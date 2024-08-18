1cc0fe466abfceb0906b04111ca7d342e4ff0ca3
Venugopal Pandey <Venugopal.Pandey@censa.in>
2024-08-18 23:39:48 +0530

    createUserDocuments in the database

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc, Firestore } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDBs2zNsHmLQQ0jnVKGkLw47Xl04J_Ivpw",
@@ -22,8 +24,38 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: "select_account"
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // it helps us to check whether the instance exists in db or not and also allows us to access the data
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if(!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message)
+    }
+  }
+
+  return userDocRef;
+}
